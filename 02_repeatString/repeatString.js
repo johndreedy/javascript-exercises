1a24e340fbfebbbe925fb82df7c89a4b8c422d37
John Reedy <johnreedy01@gmail.com>
2023-10-07 02:48:44 +0100

    Complete javascript exercise 2 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    array = [];
+        if (num > -1) {
+        for (i=0; i < num; i++) {
+            array.push(string);
+        }
+        return array.join("");
+    } else {
+        return 'ERROR';
+    }
+}
 
-};
+console.log(repeatString("Hi", 0));
 
 // Do not edit below this line
 module.exports = repeatString;
