4fe35efa26d836e5c7e17c7825a66ac6162ac828
John Reedy <johnreedy01@gmail.com>
2023-10-07 18:47:01 +0100

    Complete javascript exercise 3 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split('').reverse().join('');
+    return stringArray;
 };
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
