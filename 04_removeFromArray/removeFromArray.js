0a2fa2042b7ca7464ce228ff4dc8ab8b2a150bc3
John Reedy <johnreedy01@gmail.com>
2023-10-08 01:12:53 +0100

    Complete javascript exercise 4 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (array, ...strings) {
+    return array.filter(string => !strings.includes(string));
+} 
+removeFromArray(["Hello", "World", "!"], "World", "!");
 
 // Do not edit below this line
 module.exports = removeFromArray;
