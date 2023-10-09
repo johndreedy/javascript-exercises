f60d239ea4c39ec06a3f9f48b7b5d55cd23078c6
John Reedy <johnreedy01@gmail.com>
2023-10-09 22:39:12 +0100

    Complete javascript exercise 5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,41 @@
-const sumAll = function() {
+function calculateMinMax (number1, number2) {
+    let min = 0;
+    let max = 0;
+    if (number1 > number2) {
+        min = number2;
+        max = number1;
+    } else if (number1 < number2) {
+        min = number1;
+        max = number2;
+    } else {
+        return 'ERROR';
+    }
+    return [min, max];
+}
 
-};
+const sumAll = function(min, max) {
+
+    if (typeof min == 'number' && typeof max == 'number') {
+        let minMaxArray = calculateMinMax(min, max);
+        min = minMaxArray[0];
+        max = minMaxArray[1];
+
+        let sum = 0;
+        for (let i=min; i <= max; i++) {
+            sum += i;
+        } 
+        if (sum < 0) {
+            return 'ERROR'
+        } else {
+            return sum;
+        }
+
+    } else {
+        return 'ERROR';
+    }
+}
+
+console.log(sumAll(2, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
