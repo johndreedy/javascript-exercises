be0354a42f5a84bcfeea794a476d2dadc47432b0
John Reedy <johnreedy01@gmail.com>
2023-10-10 14:47:46 +0100

    Complete javascript exercise 6 leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    fourLeapYear = year / 4;
+    hundredLeapYear = year / 100;
+    fourHundredLeapYear = year / 400;
+    if (Number.isInteger(fourLeapYear)) {
+        if (Number.isInteger(hundredLeapYear)) {
+            if (Number.isInteger(fourHundredLeapYear)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
 
-};
+console.log(leapYears(2000));
 
 // Do not edit below this line
 module.exports = leapYears;
