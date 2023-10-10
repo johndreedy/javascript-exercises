1b1528d5fab73fd2b825be9b8648007489e61367
John Reedy <johnreedy01@gmail.com>
2023-10-10 15:03:54 +0100

    Complete javascript exercise 7 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(integer) {
+  integer -= 32;
+  integer /= 1.8;
+  integer = Math.round(integer * 10) / 10;
+  return integer;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(integer) {
+  integer *= 1.8;
+  integer += 32;
+  integer = Math.round(integer * 10) / 10;
+  return integer;
 };
+
+console.log(convertToCelsius(-100));
+
+console.log(convertToFahrenheit(50));
 
 // Do not edit below this line
 module.exports = {
